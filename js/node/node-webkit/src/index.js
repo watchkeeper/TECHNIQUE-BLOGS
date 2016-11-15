d1bf2e010523c2505163bad5c55156dbed854149
modle_sherlock <modle_sherlock@163.com>
2016-11-15 18:29:21 +0800

    closer,but async in initail state fucing puzzle me , and crypto identify

--- js/node/node-webkit/src/index.js
+++ js/node/node-webkit/src/index.js
@@ -1,10 +1,17 @@
 /*hybrid*/
 const fs = require('fs')
+const db = require('./src/mongo.js')
+const util = require('util')
 const user_record = './user.json'
 
 class Index{
 	constructor(){
-		let [router] = []
+		let [router,initial] = []
+		
+		// db.find('plan',{}).then((msg)=>{
+		// 	this.initial = msg
+		// 	alert(util.inspect(this.initial))
+		// })
 		//fuck if i put beginRouter in checkJsonDoc callback would loop jump window fucking!
 		this.beginRouter()
 		this.checkJsonDoc(user_record).then((flag)=>{})
@@ -23,22 +30,37 @@ class Index{
 					// alert('fuck')
 					const inputerUser = this.userMsg.user
 					const inputerPswd = this.userMsg.pswd
-					if( inputerUser.length<6 || inputerPswd.length<6 ){
-						alert("username or password must more than 5 bytes")
+					if( inputerUser.length<5 || inputerPswd.length<5 ){
+						alert("username or password must more than 4 bytes")
 					}else{
-						const data = fs.readFileSync(user_record)
-						const obj = JSON.parse(data)
-						const username = obj.user
-						const password= obj.pswd
-						alert(username)
-						alert(password)
-						alert(this.userMsg.user)
-						alert(this.userMsg.pswd)
-						if(username== inputerUser && password==inputerPswd){
-							alert('log in successfully!')
-						}else{
-							alert('username or password is error!!')
-						}
+						/*remore save judge*/
+						db.find('user',{username:inputerUser})
+						.then((msg)=>{
+							// alert(util.inspect(msg))
+							if(!msg){
+								alert('Your username is not right!')
+							}else{
+								if(inputerPswd!=msg.password){
+									alert('Your password is not right!')
+								}else{
+									alert('Log in successfully!!!')
+									alert(window.location.href)
+									const root_url = window.location.href.split('#/').shift()
+									alert(util.inspect(root_url))
+									window.location.href=root_url+'#/'
+								}
+							}
+						})
+						/*local json document save judge*/
+						// const data = fs.readFileSync(user_record)
+						// const obj = JSON.parse(data)
+						// const username = obj.user
+						// const password= obj.pswd
+						// if(username== inputerUser && password==inputerPswd){
+						// 	alert('log in successfully!')
+						// }else{
+						// 	alert('username or password is error!!')
+						// }
 					}
 				}
 			}
@@ -62,6 +84,23 @@ class Index{
 					}else if(user.pswd !== user.Rpswd){
 						alert("the password you inputed is not equal!")
 					}else{
+						/*remote save judge*/
+						db.find('user',{username:user.user})
+						.then((msg)=>{
+							if(msg){
+								alert('already exist ! change your username!')
+							}else{
+								db.register({
+									username:user.user,
+									password:user.pswd,
+									time:Date.now()
+								}).then((msg)=>{
+									alert('register successfully')
+									window.location.reload()
+								})
+							}
+						})
+						/*local document judge
 						//limit user exist can not reigister
 						const user = fs.readFileSync(user_record)
 						if(user.length>0){
@@ -82,6 +121,7 @@ class Index{
 							alert('register successfully!!!')
 							window.location.reload()
 						})
+						*/
 					}
 				},
 				len(str){
@@ -90,9 +130,77 @@ class Index{
 			}
 		}
 	}
+	static getTime(){
+		const cur = new Date()
+		const year = cur.getFullYear()
+		const month = cur.getMonth()+1
+		const day = cur.getDate()
+		const hour = cur.getHours()
+		const minute = cur.getMinutes()
+		const second = cur.getSeconds()
+		return `${year}-${month}-${day} ${hour}:${minute}:${second}`
+	}
 	static mainTpl(){
 		return {
-			template:"#mai-tpl"
+			template:"#main-tpl",
+			data:()=>({
+				newPlan:'',
+				planList:[
+					{_id:'1',state:'state this is ',time:'2015-03-11 15:33:33',status:0},
+					{_id:'2',state:'state this is  2',time:'2015-03-11 15:33:33',status:1}
+				]
+			}),
+			computed:{
+				// planList(){
+				// 	alert()
+				// 	alert(this.initial)
+				// 	return this.initial
+				// }
+			},
+			methods:{
+				addPlan(){
+					/* insert data*/
+					const newRecord = {
+						state:this.newPlan,
+						time:Index.getTime(),
+						status:1
+					}
+					// alert(util.inspect(newRecord))
+					db.insert('plan',[newRecord]).then((msg)=>{
+						//reDraw data from db
+						alert(util.inspect(msg))
+						this.planList.push(msg)
+					})
+				},
+				finish(id){
+					const index = this.planList.findIndex((v,n)=>(
+						v._id==id
+					))
+					// alert(index)
+					db.update('plan',{_id:id},{status:0})
+					.then((msg)=>{
+						const temp = this.planList[index]
+						const newObj = {
+							_id:temp._id,
+							state:temp.state,
+							time:temp.time,
+							status:temp.status==1?0:0
+						}
+						this.planList.splice(index,1,newObj)
+						alert(`update ${msg.result.n} data`)
+					})
+
+				},
+				ridofit(id){
+					const index = this.planList.findIndex((v,n)=>(
+						v._id==id
+					))
+					db.remove('plan',{_id:id}).then((msg)=>{
+						alert('Delete successfully')
+						this.planList.splice(index,1)
+					})
+				}
+			}
 		}
 	}
 	checkJsonDoc(loginDoc){
@@ -129,7 +237,8 @@ class Index{
 	beginRouter(){
 		const routes = [
 			{path:'/login',component:Index.loginTpl(),name:'user'},
-			{path:'/register',component:Index.registerTpl(),name:'register'}
+			{path:'/register',component:Index.registerTpl(),name:'register'},
+			{path:'/',component:Index.mainTpl(),name:'main'}
 
 		]
 		this.router = new VueRouter({
@@ -151,6 +260,7 @@ class Index{
 				}
 			}
 		})
+		// this.router.push('/')
 		this.router.push('/login')
 	}
 }
---
+++ js/node/node-webkit/src/mongo.js
@@ -0,0 +1,144 @@
+const mc = require('mongodb').MongoClient
+const assert = require('assert')
+const crypto = require('crypto')
+const http = require('http')
+
+const config  =  {}
+config.ip = '120.25.74.190'
+config.port = 27017
+config.db = 'note'
+
+const url = `mongodb://${config.ip}:${config.port}/${config.db}`
+
+const sha = (str)=>{
+	const doing = crypto.createHash('sha1')
+	doing.update(str)
+	const final = doing.digest('hex')
+	return final
+}
+
+/*
+@userMsgObj format ==>  {username:"modle",password:"sherlock",time:Date.now()}
+*/
+const register = (userMsgObj)=>{
+	const promise = new Promise((resolve,reject)=>{
+		mc.connect(url,(err,db)=>{
+			assert.equal(null,err)
+			console.log("connect successful!")
+			const col = db.collection('user')
+			col.insert([
+				{
+					username:userMsgObj.username,
+					password:sha(userMsgObj.password),
+					time:userMsgObj.time
+				}
+			],{w:1},(err,result)=>{
+				assert.equal(null,err)
+				console.log(result)
+				resolve(result)
+				db.close()
+			})
+		})
+	})
+	return promise
+}
+
+/*
+@params:
+	col:collection
+	query:{}
+}
+*/
+const find = (collection,query={})=>{
+	const promise = new Promise((resolve,reject)=>{
+		mc.connect(url,(err,db)=>{
+			assert.equal(null,err)
+			const col = db.collection(collection)
+			if( !Object.keys(query).length ){
+				col.find().toArray((err,docs)=>{
+					assert.equal(null,err)
+					console.log(docs)
+					resolve(docs)
+					db.close()
+				})
+			}else{
+				col.findOne(query,(err,docs)=>{
+					assert.equal(null,err)
+					console.log(docs)
+					resolve(docs)
+					db.close()
+				})
+			}
+		})
+	})
+	return promise
+}
+
+/*
+ 	arr:[{}]
+*/
+const insert = (collection,arr)=>{
+	const promise = new Promise((resolve,reject)=>{
+		mc.connect(url,(err,db)=>{
+			assert.equal(null,err)
+			const col = db.collection(collection)
+			col.insertMany(arr,(err,result)=>{
+				assert.ok(!err)
+				console.log('insert data successfully!!!')
+				console.log(result.ops[0])
+				resolve(result.ops[0])
+				db.close()
+			})
+		})
+	})
+	return promise
+}
+// insert('plan',[{state:"hahahah,我是sherlock",status:1,time:'0000009'}])
+
+const remove = (collection,obj)=>{
+	const promise = new Promise((resolve,reject)=>{
+		mc.connect(url,(err,db)=>{
+			assert.equal(null,err)
+			const col = db.collection(collection)
+			col.removeOne(obj,(err,result)=>{
+				assert.ok(!err)
+				console.log('delete data successfully!!!')
+				console.log(result)
+				resolve(result)
+				db.close()
+			})
+		})
+	})
+	return promise
+}
+
+const update = (collection,objFilter,newObj)=>{
+	const promise = new Promise((resolve,reject)=>{
+		mc.connect(url,(err,db)=>{
+			assert.equal(null,err)
+			const col = db.collection(collection)
+			col.updateOne(objFilter,{$set:newObj},(err,result)=>{
+				assert.ok(!err)
+				console.log(result)
+				console.log('update successfully')
+				resolve(result)
+				db.close()
+			})
+		})
+	})
+	return promise
+}
+
+// update('plan',{time:'0000009'},{state:'fufufufufucckckkck'})
+
+
+module.exports = {
+	register:register,
+	find:find,
+	insert:insert,
+	remove:remove,
+	update:update
+}
+
+
+
