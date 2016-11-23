1e9de56d370015376e860f28f30281c896d0e9c0
modle_sherlock <modle_sherlock@163.com>
2016-11-23 09:39:49 +0800

    单人聊天成功,但是还是要多总结哈有的地方

--- js/node/chat/oneToOne/index.js
+++ js/node/chat/oneToOne/index.js
@@ -1,5 +1,6 @@
 const io = require('socket.io')()
 
+/*证明多个人访问这个文件的时候,是关联的,而不是同时孤立的访问,仅仅就是一份文件,所有数据都是这个文件上发生的*/
 const users = {}
 
 io.of('/chat').on('connection',(socket)=>{
@@ -15,8 +16,14 @@ io.of('/chat').on('connection',(socket)=>{
 	socket.on('chat',(from,to,msg)=>{
 		console.log(msg)
 		/*每个用户有单一的通信的信道,保存信道*/
+		/*看看是否连接了那个target用户*/
 		if(users[to]){
 			socket.emit('chat_info',`用户${to}上线,可以进行聊天`)
+			/*用户上线后我在监听信息*/
+			socket.on('post',(pFrom,pTo,msg)=>{
+				socket.emit('post_info',`${pFrom}发送给${pTo}的信息发送成功!`)
+				users[to].emit('get',pFrom,pTo,msg)
+			})
 		}else{
 			socket.emit('chat_info',`用户${to}没有上线`)
 		}
