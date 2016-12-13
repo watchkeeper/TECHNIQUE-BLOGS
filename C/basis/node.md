c64bc7565c251411f9231b861596718f79e5d994
huangrui <huangrui>
2016-12-13 15:38:46 +0800

    format node

--- C/basis/node.md
+++ C/basis/node.md
@@ -1,12 +1,18 @@
-1.打印错误消息：
-    #include <string.h>
-    char *strerror(int errnum);将errnum（通常为errno）映射为一个出错消息，并且返回此错误消息
+#1.打印错误消息：
+```C
+#include <string.h>
+char *strerror(int errnum);将errnum（通常为errno）映射为一个出错消息，并且返回此错误消息
 
-    #include<stdio.h>
-    void perror(const char *msg);基于errno的标准错误消息，产生一条错误消息，然后返回，首先输出msg的值，然后：，接着是errno对应的错误信息
+#include<stdio.h>
+void perror(const char * msg);基于errno的标准错误消息，产生一条错误消息，然后返回，首先输出msg的值，然后：，接着是errno对应的错误信息
+```
 
-2.定义宏函数
+
+#2.定义宏函数
+```C
 #define ECHO(str) do{\
                         scanf("%s",str);\
                         printf("%s",str);\
                   }while(0)
+
+```
--- C/process&thread/process/fork.c
+++ C/process&thread/process/fork.c
@@ -4,26 +4,48 @@
 #include <stdlib.h>
 #include <errno.h>
 
+void zombie();
 void _exit_1() {
     printf("this is exit func %s\n" );
 }
 
 int main(int argc, char const *argv[]) {
+    zombie();
+
+    // atexit(_exit_1); //将_exit_1 注册为自定义终止程序。
+    return 0;
+}
+
+/**
+ * 产生僵尸进程。
+ */
+void zombie()
+{
     pid_t pid;
     if((pid = fork()) < 0){
         strerror(errno);
-        exit(-1)
-    }
-    if(pid == 0){ // 子进程
-        printf("this is child process %d\n",(int)getpid() );
+        exit(-1);
+    }else if(pid == 0){ // 子进程
+        printf("(%d) create firsst  child process %d\n",getppid(),(int)getpid() );
         // dothing();
         exit(0);
     }
+
+    if((pid = fork()) < 0){
+        strerror(errno);
+        exit(-1);
+    }else if(pid == 0){ // 子进程
+        printf("(%d) create second  child process %d\n",getppid(),(int)getpid() );
+        // dothing();
+        exit(0);
+    }
+
     if ((pid = wait(NULL)) < 0) {
         printf("wait error %s\n", strerror(errno));
     }
-    atexit(_exit_1); //将_exit_1 注册为自定义终止程序。
-    return 0;
+    printf("parent (%d) timerator child process (%d)\n",getppid(),getpid() );
+    sleep(10);
+    printf("parent (%d) timerator child process (%d)\n",getppid(),getpid() );
 }
 /*fork其他的用法19*/
 void other() {
