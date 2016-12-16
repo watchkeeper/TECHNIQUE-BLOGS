bb5a8ddf4103fc00f73de30aeb9c933e57ec87a2
modle_sherlock <modle_sherlock@163.com>
2016-12-16 15:44:42 +0800

    ni ma sb

--- js/noder/child_process/index.js
+++ js/noder/child_process/index.js
@@ -1,1 +1,35 @@
-w
+const child_process = require('child_process')
+const assert=require('assert')
+const domain = require('domain')
+
+const one = domain.create()
+one.on('error',(e)=>{
+	console.log(e)
+})
+one.run(()=>{
+	const spawn = child_process.spawn
+	const spawn_process = spawn('ls',['../','-l'])
+	spawn_process.stdout.on('data',(msg)=>{
+		console.log(`stdout data is ${msg}`)
+	})
+	spawn_process.stderr.on('data',(msg)=>{
+		console.log(`error happen ${msg}`)
+	})
+	spawn_process.on('close',(code)=>{
+		console.log(`spawn_process has stoped! and code is ${code}`)
+	})
+	console.log(`pid of spawn ${spawn_process.pid}`)
+
+
+	const exec = child_process.exec
+	const exec_process = exec('ls -l ../',(err,stdout,stderr)=>{
+		try{
+			assert.ifError(err)
+		}catch(e){
+			console.log(e)
+		}
+		// throw new Error('I am not happy')
+		console.log(stdout)
+		console.log(`pid of exec ${exec_process.pid}`)
+	})
+})
--- js/noder/process/index.js
+++ js/noder/process/index.js
@@ -1,1 +1,17 @@
-w
+/*stdout stdin stderr*/
+
+/**原来stdin如此装逼**/
+process.stdin.setEncoding('utf8')
+process.stdin.on('readable',()=>{
+	const chunk = process.stdin.read()
+	if(chunk){
+		console.log(`stdin is ${chunk}`)
+		process.stdin.end()
+	}
+})
+process.stdin.on('end',()=>{
+	process.stdout.write('stdin is come to end')
+})
+/*简直就是输出*/
+process.stdout.write(`I am pure stdout`)
+process.stderr.write(`I am pure stderr \n`)
